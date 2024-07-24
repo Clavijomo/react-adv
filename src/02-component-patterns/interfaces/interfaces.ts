71c3a33d92ce5a61b57159525dfec0ccb554b2f5
Clavijomo <jonathanclavijo0907@gmail.com>
2024-07-23 21:45:34 -0500

    Fix conflicts

--- src/02-component-patterns/interfaces/interfaces.ts
+++ src/02-component-patterns/interfaces/interfaces.ts
@@ -18,7 +18,6 @@ export interface ProductContextProps {
     product: Product;
 }
 
-
 export interface ProductCardHOCProps {
     ({ children, product }: ProductCardProps ):JSX.Element,
     Title: ({ title }: { title?: string }) => JSX.Element,
